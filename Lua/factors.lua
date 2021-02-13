b80c2725bf720b87ea8502e6afeabddce85c0140
Simon Eason <sie.eason@gmail.com>
2021-02-13 15:35:53 +0800

    added lua factors benchmark

---
+++ Lua/factors.lua
@@ -0,0 +1,11 @@
+start = os.clock()
+
+num = 2000000000
+
+for i = 1, num+1 do
+	if num % i == 0 then
+		print(i.. " is a factor of "..num)
+	end
+end
+
+print(string.format("Time taken: %.2f\ns", os.clock() - start))
