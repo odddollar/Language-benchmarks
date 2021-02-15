dfdc9dfbdbf482965969e93d700b272849cb6a9b
Andrea Manzini <ilmanzo@gmail.com>
2021-02-15 09:40:02 +0100

    format source code

--- Crystal/factors.cr
+++ Crystal/factors.cr
@@ -2,10 +2,8 @@ start = Time.monotonic
 
 NUM = 2000000000
 
-1.upto NUM do |i| 
-    puts "#{i} is a factor of #{NUM}" if NUM%i == 0 
+1.upto NUM do |i|
+  puts "#{i} is a factor of #{NUM}" if NUM % i == 0
 end
 
-puts "Finished in:",Time.monotonic-start
-
-
+puts "Finished in:", Time.monotonic - start
--- Crystal/primes.cr
+++ Crystal/primes.cr
@@ -1,16 +1,14 @@
 start = Time.monotonic
 
+primes = [] of Int32
 
-primes=[] of Int32
-
-0.upto 15000 do |i| 
-    factors = [] of Int32
-    1.upto i do |j| 
-        factors << j if i%j == 0
-    end
-    primes << i if factors.size==2
+0.upto 15000 do |i|
+  factors = [] of Int32
+  1.upto i do |j|
+    factors << j if i % j == 0
+  end
+  primes << i if factors.size == 2
 end
-puts primes 
+puts primes
 
-puts "Finished in:",Time.monotonic-start
-
+puts "Finished in:", Time.monotonic - start
